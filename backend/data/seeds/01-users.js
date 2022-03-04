df20a537af8067d8de401384baf884e081eb4804
Amethyst Willia <zmithic@gmail.com>
2022-03-04 16:29:42 -0600

    Added 2 users to 01-users seed

---
+++ backend/data/seeds/01-users.js
@@ -0,0 +1,12 @@
+
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        { username: 'test_', password: '$2a$08$UU4MfqC8Vz87m/wGBunivumy1uK/Kqa1rYA6.5P.tv6taYTR2UTXq' },
+        { username: 'beep', password: '$2a$08$/a2P1/2zrxYkPreeiOAIb.ggitoWqB2jDMbGvw2SKApJqcbBNrFoa '},
+      ]);
+    });
+};
